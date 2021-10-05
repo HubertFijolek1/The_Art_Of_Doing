103c5f53e7aadff44b17d9a9ea0a61ea29ef4b8b
HubertFijolek1 <hubertfijolek1@gmail.com>
2021-10-05 16:39:31 +0200

    Course Exercise

---
+++ 1.Basic_Data_Types/1.Letter-counter-app.py
@@ -0,0 +1,13 @@
+print("Welcome to the Letter Counter App")
+name = input("What is your name: ").title().strip()
+print("Hello, " + name + "!" )
+print("I will count the number of times that a specific letter occurs in a message . \n")
+message = input("Please enter a message: ").lower()
+letter = input("Which letter would you like to count the occurrences of: ").lower()
+i=0
+for l in message:
+    if letter == l: 
+        i+=1
+    
+print(name + "your master has " + str(i)+ "'s in it")
+
